98472e8b349c3ca35ed1ed0e7c3f4f2b244a457f
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-05-06 00:44:15 -0400

    Add youtube channel link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import useServiceWorker from 'hooks/useServiceWorker'
 
 import {
+  Icon,
   Link,
   Text,
   Stack,
   Image,
   Heading
 } from '@chakra-ui/react'
+
+import { AiFillYoutube } from 'react-icons/ai'
 
 import Logo from 'images/logo.svg'
 import Dev from 'images/dev.svg'
@@ -17,7 +20,12 @@ export default function App () {
   return (
     <Stack height='100%' justifyContent='center' alignItems='center' spacing='8' color='gray.400'>
       <Image src={Dev} width='300px' alt='Developer' />
-      <Image src={Logo} width='500px' alt='Codepiercer' />
+      <Stack alignItems='center'>
+        <Image src={Logo} width='500px' alt='Codepiercer' />
+        <Link href='https://www.youtube.com/channel/UCbCSOBpNa23ppV-_s2Ce_og/featured' isExternal>
+          <Icon as={AiFillYoutube} w={8} h={8} color='red' />
+        </Link>
+      </Stack>
       <Stack alignItems='center'>
         <Heading fontSize='md'>by</Heading>
         <Heading as={Link} isExternal href='https://jana19.dev' fontSize='3xl'>Jana Rajakumar</Heading>
